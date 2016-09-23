5aa4dbe88f198401f4b8330269ab64ce65c0488e
Neil Rajendran <rajendranneil19@gmail.com>
2016-09-23 19:15:45 +0000

    Done.

--- loops.js
+++ loops.js
@@ -10,8 +10,18 @@ for (let i = 0; i < 25; i++){
     }
 
 
-function whileloop(n){
+function whileLoop(n){
   while (n > 0){
     console.log(--n)}
     return "done"
   }
+
+function doWhileLoop(array){
+  function maybeTrue() {
+  return Math.random() >= 0.5
+}
+  do {
+array = array. slice(1);
+} while (array.length > 0 && maybeTrue());
+return array
+}
